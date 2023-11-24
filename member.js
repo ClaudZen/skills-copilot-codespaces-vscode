7d42287475559576c074395ace1766402ffe4ac2
Claudio Anticoi <55770366+ClaudZen@users.noreply.github.com>
2023-11-24 03:33:52 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,35 @@
+function skillsMember() {
+    var skills = {
+        'skills': [
+            {
+                'name': 'HTML5',
+                'value': '90%'
+            },
+            {
+                'name': 'CSS3',
+                'value': '90%'
+            },
+            {
+                'name': 'JavaScript',
+                'value': '70%'
+            },
+            {
+                'name': 'jQuery',
+                'value': '70%'
+            },
+            {
+                'name': 'PHP',
+                'value': '60%'
+            },
+            {
+                'name': 'MySQL',
+                'value': '60%'
+            },
+            {
+                'name': 'Photoshop',
+                'value': '40%'
+            }
+        ]
+    };
+    return skills;
+}
